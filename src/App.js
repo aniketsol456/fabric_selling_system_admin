ab43eefedf9512402cf5efd0423a4c971e5d13ed
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-11-29 19:01:58 +0530

    add path in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <>
         <div className='Container'>
           <Routes>
-            <Route exact path='/' element={<Login />} />
+            <Route exact path='/' element={<Login/>} />
+            <Route exact path='/sidebar' element={<Sidebar/>} />
           </Routes>
 
         </div>
