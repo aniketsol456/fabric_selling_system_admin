b3ef0fd44d59da8128941b2f99df567ae44b7492
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-11-29 19:02:10 +0530

    creating a login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div className="login-container">
-      <div className="login-card">
-        <h2 className="login-title">Admin Login</h2>
-        <form className="login-form">
-          <input type="email" placeholder="Enter your an Email" className="login-input" />
-          <input type="password" placeholder="Enter your Password" className="login-input" />
-          <button type="submit" className="login-button">LOGIN</button>
-        </form>
-      </div>
-    </div>
-  );
+    const navigate = useNavigate();
+
+    const GotoDash = () => {
+        navigate("/Sidebar");
+    }
+
+    return (
+        <div className="login-container">
+            <div className="login-card">
+                <h2 className="login-title">Admin Login</h2>
+                <form className="login-form">
+                    <input type="email" placeholder="Enter your an Email" className="login-input" />
+                    <input type="password" placeholder="Enter your Password" className="login-input" />
+                    <button type="submit" className="login-button" onClick={GotoDash}>LOGIN</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
