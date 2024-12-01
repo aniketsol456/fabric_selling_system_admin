d6a82d8f3bc42d5f009b4608f27fa295f2922242
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-12-01 18:19:51 +0530

    adding a rout for new pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Login from './Components/Login';
 import Sidebar from './Components/Sidebar';
 import Dashboard from './Components/Dashboard';
+import CustomerManagement from './Components/CustomerManagement';
+import Shipping from './Components/Shipping';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
                 <div className='mainContent'>
                   <Routes>
                     <Route path='/dashboard' element={<Dashboard/>} />
+                    <Route path='/customers' element={<CustomerManagement/>} />
+                    <Route path='/shipping' element={<Shipping/>} />
                   </Routes>
                 </div>
               </>
