42ed652e27ba02a311b4bb8570ee1c3705f87481
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-12-01 17:34:33 +0530

    change in routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
 import Sidebar from './Components/Sidebar';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
-      <>
-        <div className='Container'>
-          <Routes>
-            <Route exact path='/' element={<Login/>} />
-            <Route exact path='/sidebar' element={<Sidebar/>} />
-          </Routes>
-
-        </div>
-      </>
-
+      <div className='appContainer'>
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route
+            path='*'
+            element={
+              <>
+                <Sidebar />
+                <div className='mainContent'>
+                  <Routes>
+                    <Route path='/dashboard' element={<Dashboard/>} />
+                  </Routes>
+                </div>
+              </>
+            }
+          />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
