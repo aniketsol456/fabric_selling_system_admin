49a002847693bcea2feb1ef645b4050c204563d9
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-12-01 17:36:08 +0530

    structure change in login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -10,14 +10,16 @@ const Login = () => {
     }
 
     return (
-        <div className="login-container">
-            <div className="login-card">
-                <h2 className="login-title">Admin Login</h2>
-                <form className="login-form">
-                    <input type="email" placeholder="Enter your an Email" className="login-input" />
-                    <input type="password" placeholder="Enter your Password" className="login-input" />
-                    <button type="submit" className="login-button" onClick={GotoDash}>LOGIN</button>
-                </form>
+        <div className='loginPage'>
+            <div className="login-container">
+                <div className="login-card">
+                    <h2 className="login-title">Admin Login</h2>
+                    <form className="login-form">
+                        <input type="email" placeholder="Enter your an Email" className="login-input" />
+                        <input type="password" placeholder="Enter your Password" className="login-input" />
+                        <button type="submit" className="login-button" onClick={GotoDash}>LOGIN</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
