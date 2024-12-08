7d6da4b0225c23b2beeeed6e3a7e8e8644b330b7
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-12-08 11:12:40 +0530

    make a structure

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -8,18 +8,38 @@ import {
   FaUserShield,
   FaTags,
   FaTruck,
+  FaChevronDown,
+  FaChevronUp,
 } from "react-icons/fa";
 import "./Sidebar.css";
 import Logo from "../assets/images/logo_2.png";
 
 const Sidebar = ({ isOpen }) => {
+  const [storeSubmenuOpen, setStoreSubmenuOpen] = useState(false);
 
-  
+  const toggleStoreSubmenu = () => {
+    setStoreSubmenuOpen(!storeSubmenuOpen);
+  };
+
   const menuItems = [
     { name: "Dashboard", icon: <FaTachometerAlt />, link: "/dashboard" },
     { name: "Manage Admins", icon: <FaUserShield />, link: "/admins" },
     { name: "Order Management", icon: <FaFileInvoice />, link: "/orders" },
-    { name: "Store Management", icon: <FaBoxes />, link: "/fabrics" },
+    {
+      name: "Store Management",
+      icon: <FaBoxes />,
+      link: "/fabrics", // main link if needed
+      hasSubmenu: true,
+      submenu: [
+        { name: "Fabrics", link: "/fabrics" },
+        { name: "Fabrics Weight", link: "/fabrics-weight" },
+        { name: "Fabrics Content", link: "/fabrics-content" },
+        { name: "Fabrics Type", link: "/fabrics-type" },
+        { name: "Fabrics Width", link: "/fabrics-width" },
+        { name: "Fabrics Design", link: "/fabrics-design" },
+        { name: "Fabrics Color", link: "/fabrics-color" },
+      ],
+    },
     { name: "Customer Management", icon: <FaUsers />, link: "/customers" },
     { name: "Shipping", icon: <FaTruck />, link: "/shipping" },
     { name: "Coupons", icon: <FaTags />, link: "/couponview" },
@@ -40,14 +60,46 @@ const Sidebar = ({ isOpen }) => {
       <ul className="menu">
         {menuItems.map((item, index) => (
           <li key={index} className="menuItem">
-            <NavLink
-              to={item.link}
-              className="menuItemContent"
-              activeClassName="active"
-            >
-              <div className="icon">{item.icon}</div>
-              <span>{item.name}</span>
-            </NavLink>
+            {item.hasSubmenu ? (
+              <>
+                {/* Collapsible Menu */}
+                <div
+                  className="menuItemContent"
+                  onClick={toggleStoreSubmenu}
+                >
+                  <div className="icon">{item.icon}</div>
+                  <span>{item.name}</span>
+                  <div className="chevron">
+                    {storeSubmenuOpen ? <FaChevronUp /> : <FaChevronDown />}
+                  </div>
+                </div>
+                {/* Submenu Items */}
+                {storeSubmenuOpen && (
+                  <ul className="submenu">
+                    {item.submenu.map((subItem, subIndex) => (
+                      <li key={subIndex} className="submenuItem">
+                        <NavLink
+                          to={subItem.link}
+                          className="submenuItemContent"
+                          activeClassName="active"
+                        >
+                          {subItem.name}
+                        </NavLink>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </>
+            ) : (
+              <NavLink
+                to={item.link}
+                className="menuItemContent"
+                activeClassName="active"
+              >
+                <div className="icon">{item.icon}</div>
+                <span>{item.name}</span>
+              </NavLink>
+            )}
           </li>
         ))}
       </ul>
